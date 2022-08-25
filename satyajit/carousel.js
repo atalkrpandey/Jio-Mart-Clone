7f1b538f9140b2b870646f253040350fb40d3999
unknown <roysatyajit772912@gmail.com>
2022-08-25 21:03:26 +0530

    day-3

--- satyajit/carousel.js
+++ satyajit/carousel.js
@@ -131,3 +131,132 @@ if(currentScrollPosition <= maxScroll){
     sCont.style.left = currentScrollPosition + 'px';
     
 }
+
+
+
+// ///////////////////Top Deals /
+
+
+currentScrollPosition_2 = 0;
+let scrollAmount_2 = 280;
+
+const sCont_2 = document.querySelector(".Satyajit-storys-container")
+const hScroll2 = document.querySelector(".Satyajit-horizontal-scroll");
+const btnScrollLeft_2 = document.querySelector('#Satyajit-btn-scroll-left')
+const btnScrollRight_2 = document.querySelector('#Satyajit-btn-scroll-right')
+
+btnScrollLeft_2.style.opacity = '0.5'
+
+
+let maxScroll_2 = -sCont_2.offsetWidth + hScroll2.offsetWidth;
+
+
+
+function scrollHorizontally_2(val){
+    currentScrollPosition_2 += (val * scrollAmount_2);
+if(currentScrollPosition_2 > 0){
+    currentScrollPosition_2 = 0;
+    btnScrollLeft_2.style.opacity = '0.3'
+}else{
+    btnScrollLeft_2.style.opacity = '1'
+}
+if(currentScrollPosition_2 <= maxScroll_2){
+    currentScrollPosition_2 = maxScroll_2;
+    btnScrollRight_2.style.opacity = '0.5'
+}else{
+    btnScrollRight_2.style.opacity = '1'
+}
+    sCont_2.style.left = currentScrollPosition_2 + 'px';
+    
+}
+
+
+// /////////////////START OF FINANCE SECTION
+
+
+currentScrollPosition_3 = 0;
+let scrollAmount_3 = 410;
+let id;
+const sCont_3 = document.querySelector(".Sat-storys-container")
+const hScroll_3 = document.querySelector(".Sat-horizontal-scroll");
+const btnScrollLeft_3 = document.querySelector('#Sat-btn-scroll-left')
+const btnScrollRight_3 = document.querySelector('#Sat-btn-scroll-right')
+
+btnScrollLeft_3.style.opacity = '0.5'
+
+
+let maxScroll_3 = -sCont_3.offsetWidth + hScroll_3.offsetWidth;
+
+
+
+function scrollHorizontally_3(val){
+    currentScrollPosition_3 += (val * scrollAmount_3);
+    console.log(currentScrollPosition_3,val, scrollAmount_3)
+if(currentScrollPosition_3 > 0){
+    currentScrollPosition_3 = 0;
+    btnScrollLeft_3.style.opacity = '0.3'
+}else{
+    btnScrollLeft_3.style.opacity = '1'
+}
+if(currentScrollPosition_3 <= maxScroll_3){
+    currentScrollPosition_3 = maxScroll_3;
+    btnScrollRight_3.style.opacity = '0.5'
+}else{
+    btnScrollRight_3.style.opacity = '1'
+}
+    sCont_3.style.left = currentScrollPosition_3 + 'px';
+    
+}
+
+
+
+id = setInterval(()=>{
+    currentScrollPosition_3 += (-1 * scrollAmount_3);
+if(currentScrollPosition_3 <= -4400){
+    currentScrollPosition_3 = 0;
+    btnScrollLeft_3.style.opacity = '0.3'
+}else{
+    btnScrollLeft_3.style.opacity = '1'
+}
+    sCont_3.style.left = currentScrollPosition_3 + 'px';
+},4000)
+
+
+
+/////////////////////Startin OF shop By Ctaegory////////////////////
+
+
+
+currentScrollPosition_4 = 0;
+let scrollAmount_4 =180;
+
+const sCont_4 = document.querySelector(".Satyajit-2-storys-container")
+const hScroll_4 = document.querySelector(".Satyajit-2-horizontal-scroll");
+const btnScrollLeft_4 = document.querySelector('#Satyajit-2-btn-scroll-left')
+const btnScrollRight_4 = document.querySelector('#Satyajit-2-btn-scroll-right')
+
+btnScrollLeft_4.style.opacity = '0.5'
+
+
+// let maxScroll_4 = -sCont_4.offsetWidth + hScroll2.offsetWidth;
+let maxScroll_4 = -560;
+
+
+function scrollHorizontally_4(val){
+    currentScrollPosition_4 += (val * scrollAmount_4);
+    console.log(currentScrollPosition_4)
+if(currentScrollPosition_4 > 0){
+    currentScrollPosition_4 = 0;
+    btnScrollLeft_4.style.opacity = '0.3'
+}else{
+    btnScrollLeft_4.style.opacity = '1'
+}
+if(currentScrollPosition_4 <= maxScroll_4){
+    currentScrollPosition_4 = maxScroll_4;
+    btnScrollRight_4.style.opacity = '0.5'
+}else{
+    btnScrollRight_4.style.opacity = '1'
+}
+    sCont_4.style.left = currentScrollPosition_4 + 'px';
+    
+}
---
+++ satyajit/footer Component/footer.js
@@ -0,0 +1,54 @@
+function footer (){
+    return `
+    <div id="sa-footer-container">
+    <div class="sa-footer-box">
+        <h5>MOST POPULAR CATEGORIES</h5>
+        <p>Staples</p>
+        <p>Beverages</p>
+        <p>Personal Care</p>
+        <p>Home Care</p>
+        <p>Fruits and Vegetables</p>
+        <p>Baby Care</p>
+        <p>Snacks & Branded Foods</p>
+        <p>Dairy & Bakery</p>
+    </div>
+    <div class="sa-footer-box">
+        <h5>CUSTOMER SERVICES</h5>
+        <p>About Us</p>
+        <p>FAQ</p>
+        <p>Terms and conditions</p>
+        <p>Privacy policy</p>
+        <p>E-waste Policy</p>
+        <p>Cancellation and return policy</p>
+
+    </div>
+    <div class="sa-footer-box">
+        <h5>CONTACT US</h5>
+        <p>WhatsApp us : <span style="color: rgb(0, 179, 255);">70003 70003</span> </p>
+        <p>Call Us :<span style="color: rgb(0, 179, 255);">1800 890 1222</span> </p>
+        <p>8:00 AM to 8:00 PM, 365 days</p>
+        <p>Please note that you are accessing the BETA Version of <a style="color: rgb(0, 179, 255);"
+                href="">www.jiomart.com</a> </p>
+        <p>Should you encounter any bugs, glitches, lack of functionality, delayed deliveries, billing errors or
+            other problems on the beta website, please email us on <a style="color: rgb(0, 179, 255);"
+                href="cs@jiomart.com"></a> </p>
+
+
+        <div id="download">
+            <h5>DOWNLOAD APP</h5>
+            <a href=""> <img src="https://www.jiomart.com/images/cms/wysiwyg/app-icons/play_store.png"
+                    alt=""></a>
+            <a href=""> <img src="https://www.jiomart.com/images/cms/wysiwyg/app-icons/ios_store.png"
+                    alt=""></a>
+        </div>
+    </div>
+</div>
+<div id="sa-footer-end">
+    <p>Best viewed on Microsoft Edge 81+, Mozilla Firefox 75+, Safari 5.1.5+, Google Chrome 80+</p>
+    <p>© 2022 All rights reserved. Reliance Retail Ltd.</p>
+</div>
+    `
+
+}
+
+export default footer;
